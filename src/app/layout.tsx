c5c17fc8dd87e0cf1a3303bd6fb04a95b379f577
Amanda Oliveira <amandaol7b@gmail.com>
2025-06-28 23:54:55 -0300

    init: init project and add Header component

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Mulish } from "next/font/google";
+import "./globals.css";
+
+const mulish = Mulish({
+  variable: "--font-mulish",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "uEye",
+  description: "Landing page for uEye",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body className={`${mulish.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import "../styles/header.css"; 
+
+export default function Header() {
+  return (
+    <div className="header">
+      <nav className="navbar">
+        <Image
+          src="/LOGO-UEYE.svg"
+          alt="uEye Logo"
+          width={52}
+          height={52}
+          className="logo"
+        />
+        <button className="demo-button">Receba a demo</button>
+      </nav>
+      <div className="container">
+        <div className="content">
+          <div className="text-content">
+            <h1 className="title">A Nova era do UX/UI Design</h1>
+            <p className="subtitle">
+              Use o poder do rastreio ocular real e da IA preditiva para gerar
+              heatmaps automáticos, avaliar suas telas e otimizar a experiência
+              do usuário com base em dados visuais.
+            </p>
+          </div>
+          <div className="send-email">
+            <input type="text" placeholder="Digite seu e-mail" className="email-input" />
+            <button className="demo-button">Receba a demo</button>
+          </div>
+        </div>
+        <Image
+          src="/ueye-desktop.svg"
+          alt="Header Image"
+          width={1505}
+          height={510}
+          className="header-image"
+        />
+      </div>
+    </div>
+  );
+}
